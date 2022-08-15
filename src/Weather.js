d5af8519e1448f2ecff75a20c67c197aadcc5493
standarthoid <90977509+standarthoid@users.noreply.github.com>
2022-08-15 11:09:56 +0200

    updated SearchEngine

--- src/Weather.js
+++ src/Weather.js
@@ -2,12 +2,12 @@ import React, { useState } from "react";
 import axios from "axios";
 import Forecast from "./Forecast.js";
 import Footer from "./Footer.js";
-import NiceDate from "./NiceDate.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./Weather.css";
-
+import WeatherInfo from "./Weatherinfo.js";
 export default function Weather(props) {
   const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
   function handleResponse(response) {
     console.log(response.data);
     setWeatherData({
@@ -21,6 +21,19 @@ export default function Weather(props) {
       date: new Date(response.data.dt * 1000),
     });
   }
+
+  function search() {
+    const apiKey = "b68955b53e53008eb9772143fe4d18a1";
+    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
   if (weatherData.ready) {
     return (
       <div className="Weather">
@@ -29,87 +42,21 @@ export default function Weather(props) {
             <div className="weather_position">
               <h2 className="how_weather">How's the weather in</h2>
               <div className="App">
-                <form id="search-form">
+                <form id="search-form" onSubmit={handleSubmit}>
                   <input
                     type="text"
                     placeholder="Search for a city"
                     autocomplete="off"
                     autofocus="on"
                     id="search-text-input"
+                    onChange={handleCityChange}
                   />
                   <input type="submit" className="btn btn-info" value="Go!" />
                   <button className="location" id="location">
                     Current Location
                   </button>
                 </form>
-              </div>
-              <div>
-                <div className="current_location">Location</div>
-                <div className="row">
-                  <div className="col">
-                    <div className="date">
-                      <NiceDate date={weatherData.date} />
-                    </div>
-                    <div className="description-weather text-capitalize">
-                      {weatherData.description}
-                    </div>
-                    <div className="currently" id="current-city">
-                      {weatherData.city}
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="WeatherIcon">
-                <img
-                  src={weatherData.iconUrl}
-                  alt={weatherData.description}
-                  id="icon"
-                />
-              </div>
-              <div className="col">
-                <span className="temperature" id="temperature">
-                  {weatherData.temperature}
-                </span>
-                <span className="temperature_celsius">
-                  <a
-                    href="https://www.google.com"
-                    className="active"
-                    id="celsius"
-                  >
-                    °C
-                  </a>{" "}
-                  |{" "}
-                </span>
-                <span className="temperature_fahrenheit">
-                  <a href="https://www.google.com" id="fahrenheit">
-                    {" "}
-                    °F
-                  </a>
-                </span>
-              </div>
-              <div className="row">
-                <div className="col">
-                  <div className="daytime"></div>
-                </div>
-                <div className="col"></div>
-                <div className="col">
-                  <div className="precipitation" id="precipitation">
-                    {""}
-                  </div>
-                  <div className="humidity" id="humidity">
-                    Humidity: {weatherData.humidity}%
-                  </div>
-                  <div className="wind" id="wind">
-                    Wind: {weatherData.wind} km/h
-                  </div>
-                </div>
-              </div>
-              <div className="row">
-                <div className="col">
-                  <div className="current_weather" id="weather_conditions">
-                    {weatherData.currently}
-                  </div>
-                </div>
+                <WeatherInfo data={weatherData} />
               </div>
             </div>
           </div>
@@ -121,9 +68,7 @@ export default function Weather(props) {
       </div>
     );
   } else {
-    const apiKey = "b68955b53e53008eb9772143fe4d18a1";
-    let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(handleResponse);
+    search();
     return "Loading...";
   }
 }
---
+++ src/Weatherinfo.js
@@ -0,0 +1,68 @@
+import React from "react";
+import NiceDate from "./NiceDate.js";
+export default function WeatherInfo(props) {
+  return (
+    <div className="WeatherInfo">
+      <div>
+        <div className="current_location">Location</div>
+        <div className="row">
+          <div className="col">
+            <div className="date">
+              <NiceDate date={props.data.date} />
+            </div>
+            <div className="description-weather text-capitalize">
+              {props.data.description}
+            </div>
+            <div className="currently" id="current-city">
+              {props.data.city}
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="WeatherIcon">
+        <img src={props.data.iconUrl} alt={props.data.description} id="icon" />
+      </div>
+      <div className="col">
+        <span className="temperature" id="temperature">
+          {props.data.temperature}
+        </span>
+        <span className="temperature_celsius">
+          <a href="https://www.google.com" className="active" id="celsius">
+            °C
+          </a>{" "}
+          |{" "}
+        </span>
+        <span className="temperature_fahrenheit">
+          <a href="https://www.google.com" id="fahrenheit">
+            {" "}
+            °F
+          </a>
+        </span>
+      </div>
+      <div className="row">
+        <div className="col">
+          <div className="daytime"></div>
+        </div>
+        <div className="col"></div>
+        <div className="col">
+          <div className="precipitation" id="precipitation">
+            {""}
+          </div>
+          <div className="humidity" id="humidity">
+            Humidity: {props.data.humidity}%
+          </div>
+          <div className="wind" id="wind">
+            Wind: {props.data.wind} km/h
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col">
+          <div className="current_weather" id="weather_conditions">
+            {props.data.currently}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
